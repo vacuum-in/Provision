772d46599385b6a3ceae0234e5ec6e8e4f2e376f
Sergey <myemail@mail.com>
2019-01-15 00:31:46 +0200

    msiexec

--- Install-AzurePS.ps1
+++ Install-AzurePS.ps1
@@ -1,4 +1,4 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri "https://github.com/vacuum-in/Provision/raw/master/PackageManagement_x64.msi" -OutFile "C:\PackageManagement_x64.msi"
-Invoke-Expression -Command "C:\PackageManagement_x64.msi /quite"
+Invoke-Expression -Command "msiexec /i  C:\PackageManagement_x64.msi /qn"
 powershell -Command "Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force; Install-Module -Name Azure -RequiredVersion 5.7.0"
