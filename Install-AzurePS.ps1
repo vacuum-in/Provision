1b4af629291c0df45e24e725fe8c35c84e937fc9
vacuum-in <35137937+vacuum-in@users.noreply.github.com>
2019-01-15 12:02:20 +0200

    Update Install-AzurePS.ps1

--- Install-AzurePS.ps1
+++ Install-AzurePS.ps1
@@ -3,3 +3,4 @@ Invoke-WebRequest -Uri "https://github.com/vacuum-in/Provision/raw/master/Packag
 Invoke-Expression -Command "msiexec /i  C:\PackageManagement_x64.msi /qn"
 Start-Sleep -Seconds 5
 powershell -Command "Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force; Install-Module -Name AzureRM -RequiredVersion 5.7.0 -force "
+write-host "Ok"
