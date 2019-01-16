c03f9c720fad761fb9778a1499e44ac61f916741
vacuum-in <35137937+vacuum-in@users.noreply.github.com>
2019-01-16 14:01:16 +0200

    Update Install-AzurePS.ps1

--- Install-AzurePS.ps1
+++ Install-AzurePS.ps1
@@ -5,7 +5,7 @@ Start-Sleep -Seconds 5
 powershell -Command "Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force; Install-Module -Name AzureRM -RequiredVersion 5.7.0 -force "
 if(Get-Partition | Where-Object -Property DriveLetter -EQ "C")
 {
- Resize-Partition -DriveLetter "C" -Size (15GB)
+ Resize-Partition -DriveLetter "C" -Size (16GB)
 }
 
 write-host "Ok"
