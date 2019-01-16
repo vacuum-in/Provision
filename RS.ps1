6d1e956707598809b4404879f1132caac0e89d68
vacuum-in <35137937+vacuum-in@users.noreply.github.com>
2019-01-16 16:19:08 +0200

    Add files via upload

---
+++ RS.ps1
@@ -0,0 +1,45 @@
+﻿    param (
+        [string]$siteName,
+        [string]$connectionString,
+        [string]$DBUserName,
+        [string]$DBPassword,
+          [string]$connectionStringName = "BookContext",
+        [string]$ResourceGroupName
+    )
+    Import-Module WebAdministration
+    #New-Item -Path "C:\" -Name TEstFile.txt -ItemType File -ErrorAction SilentlyContinue
+    $DNS = (Get-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName)
+    $OUT = $DNS | Sort-Object -Property Timestamp -Descending | Select-Object -First 1
+    $DNSname = $OUT.Outputs.sqldns.Value
+    $DNSname  | Out-File -FilePath C:\TEstFile.txt -append
+    
+    $connectionString = $connectionString -replace "!DBTOREPLACE!", $DNSname  
+    $connectionString = $connectionString -replace "!USERTOREPLACE!", $DBUserName
+    $connectionString = $connectionString -replace "!PASSTOREPLACE!", $DBPassword
+    $connectionString  | Out-File -FilePath C:\TEstFile.txt -append
+    #$BooksContext
+    #write-host "##vso[task.setvariable variable=BooksContext]$connectionString"
+
+    $webPath = (Get-Website -Name $siteName).Physicalpath
+    $webConfFile = "$webPath" + "\Web.config"
+
+	$config = [xml](Get-Content -LiteralPath $webConfFile)
+
+    $config.Configuration.connectionStrings
+
+	$connStringElement = $config.SelectSingleNode("configuration/connectionStrings/add[@name='$connectionStringName']")
+
+    if($connStringElement) 
+    {
+
+        $connStringElement.connectionString = $connectionString
+
+    $config.Save($webConfFile)
+    }
+
+    else
+    {
+        Write-Error "Unable to locate connection string named: $connectionStringName"
+    }
+
+
